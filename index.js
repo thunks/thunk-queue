8139b9c48d8c7eca0cb3a1eb7c3b56ebd538f3f7
zensh <admin@zensh.com>
2015-05-09 16:16:22 +0800

    update with thunks@v3.0.0

--- index.js
+++ index.js
@@ -3,37 +3,37 @@
 //
 // **License:** MIT
 
-var Thunk = require('thunks')();
+var thunk = require('thunks')();
 
 module.exports = function thunkQueue(task) {
   var endQueue, queue = [], ctx = this, ended = false;
 
-  var thunk = Thunk.call(this, function (callback) {
+  var resultThunk = thunk.call(this, function(callback) {
     endQueue = callback;
   })(function(error, res) {
     if (error != null) throw error;
     return res;
   });
 
-  thunk.push = function(task) {
+  resultThunk.push = function(task) {
     if (ended) throw new Error('the queue is ended.');
     queue.push(evalThunk(ctx, task));
-    return thunk;
+    return resultThunk;
   };
 
-  thunk.end = function(task) {
-    if (task) thunk.push(task);
+  resultThunk.end = function(task) {
+    if (task) resultThunk.push(task);
     ended = true;
-    Thunk.all(queue)(endQueue);
-    return thunk;
+    thunk.all(queue)(endQueue);
+    return resultThunk;
   };
 
-  return task ? thunk.push(task) : thunk;
+  return task ? resultThunk.push(task) : resultThunk;
 };
 
 function evalThunk(ctx, task) {
-  return Thunk.call(this, task)(function (error, res) {
+  return thunk.call(this, task)(function(error, res) {
     if (error != null) throw error;
-    return arguments.length > 2 ? Thunk.digest.apply(null, arguments) : res;
+    return arguments.length > 2 ? thunk.digest.apply(null, arguments) : res;
   });
 }
