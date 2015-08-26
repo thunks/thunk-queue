a090ad9a32f756ae1b85881d74775781bc2fdd7c
zensh <admin@zensh.com>
2015-08-26 23:10:03 +0800

    update dependencies, use standard style

--- index.js
+++ index.js
@@ -1,39 +1,42 @@
-'use strict';
+'use strict'
 // **Github:** https://github.com/thunks/thunk-queue
 //
 // **License:** MIT
 
-var thunk = require('thunks')();
+var thunk = require('thunks')()
 
-module.exports = function thunkQueue(task) {
-  var endQueue, queue = [], ctx = this, ended = false;
+module.exports = function thunkQueue (task) {
+  var endQueue
+  var queue = []
+  var ctx = this
+  var ended = false
 
-  var resultThunk = thunk.call(this, function(callback) {
-    endQueue = callback;
-  })(function(error, res) {
-    if (error != null) throw error;
-    return res;
-  });
+  var resultThunk = thunk.call(this, function (callback) {
+    endQueue = callback
+  })(function (error, res) {
+    if (error != null) throw error
+    return res
+  })
 
-  resultThunk.push = function(task) {
-    if (ended) throw new Error('the queue is ended.');
-    queue.push(evalThunk(ctx, task));
-    return resultThunk;
-  };
+  resultThunk.push = function (task) {
+    if (ended) throw new Error('the queue is ended.')
+    queue.push(evalThunk(ctx, task))
+    return resultThunk
+  }
 
-  resultThunk.end = function(task) {
-    if (task) resultThunk.push(task);
-    ended = true;
-    thunk.all(queue)(endQueue);
-    return resultThunk;
-  };
+  resultThunk.end = function (task) {
+    if (task) resultThunk.push(task)
+    ended = true
+    thunk.all(queue)(endQueue)
+    return resultThunk
+  }
 
-  return task ? resultThunk.push(task) : resultThunk;
-};
+  return task ? resultThunk.push(task) : resultThunk
+}
 
-function evalThunk(ctx, task) {
-  return thunk.call(this, task)(function(error, res) {
-    if (error != null) throw error;
-    return arguments.length > 2 ? thunk.digest.apply(null, arguments) : res;
-  });
+function evalThunk (ctx, task) {
+  return thunk.call(this, task)(function (error, res) {
+    if (error != null) throw error
+    return arguments.length > 2 ? thunk.digest.apply(null, arguments) : res
+  })
 }
--- test/index.js
+++ test/index.js
@@ -1,60 +1,63 @@
-'use strict';
-/*global describe, it, before, after, beforeEach, afterEach*/
+'use strict'
+/* global describe, it */
 
-var assert = require('assert');
-var Thunk = require('thunks')();
-var thunkQueue = require('../index');
+var assert = require('assert')
+var Thunk = require('thunks')()
+var thunkQueue = require('../index')
 
 describe('thunk-queue', function () {
   it('thunkQueue()', function (done) {
-    var thunk = thunkQueue();
+    var thunk = thunkQueue()
 
-    thunk(function(err, res) {
-      assert.deepEqual(res, [1, 2, 3]);
-    })(done);
+    thunk(function (err, res) {
+      assert.strictEqual(err, null)
+      assert.deepEqual(res, [1, 2, 3])
+    })(done)
 
-    thunk.push(1);
+    thunk.push(1)
 
-    thunk.push(Thunk(2));
+    thunk.push(Thunk(2))
 
-    thunk.end(Thunk.delay(10)(function() {
-      return 3;
-    }));
-  });
+    thunk.end(Thunk.delay(10)(function () {
+      return 3
+    }))
+  })
 
   it('thunkQueue(value)', function (done) {
-    var thunk = thunkQueue(1);
+    var thunk = thunkQueue(1)
 
-    thunk.push(2);
+    thunk.push(2)
 
-    thunk.end(Thunk(3));
+    thunk.end(Thunk(3))
 
-    thunk(function(err, res) {
-      assert.deepEqual(res, [1, 2, 3]);
-    })(done);
-  });
+    thunk(function (err, res) {
+      assert.strictEqual(err, null)
+      assert.deepEqual(res, [1, 2, 3])
+    })(done)
+  })
 
   it('thunkQueue() with empty queue', function (done) {
-    var thunk = thunkQueue();
+    var thunk = thunkQueue()
 
-    thunk.end();
+    thunk.end()
 
-    thunk(function(err, res) {
-      assert.deepEqual(res, []);
-    })(done);
-  });
+    thunk(function (err, res) {
+      assert.strictEqual(err, null)
+      assert.deepEqual(res, [])
+    })(done)
+  })
 
   it('thunkQueue() throw error', function (done) {
-    var thunk = thunkQueue();
+    var thunk = thunkQueue()
 
-    thunk.push(1);
+    thunk.push(1)
 
-    thunk.end();
+    thunk.end()
 
-    assert.throws(function() {
-      thunk.push(2);
-    });
-    done();
-  });
+    assert.throws(function () {
+      thunk.push(2)
+    })
+    done()
+  })
 
-});
+})
